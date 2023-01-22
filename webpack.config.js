d1502adafb300878667f8e38afacee512325df28
Dwayne <dwaynec@iaanyware.net>
2023-01-22 13:31:22 +1000

    feat(deploy): copy redirects to dist

--- webpack.config.js
+++ webpack.config.js
@@ -83,6 +83,12 @@ module.exports = function(env, { analyze }) {
       new Dotenv({
         path: `./.env${production ? '' :  '.' + (process.env.NODE_ENV || 'development')}`,
       }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: './src/_redirects', to: '' },
+          { from: './static/blog.json', to: 'static' },
+        ],
+      }),
       analyze && new BundleAnalyzerPlugin()
     ].filter(p => p)
   }
